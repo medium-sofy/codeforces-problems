56511b090e1c71a02a7b1dc4cc0e628c06cc87b4
Mohamed Sofy <mediumsofy@gmail.com>
2025-08-18 23:39:09 +0300

    Add 617A-elephant solution

---
+++ 617A-Elephant/sol.php
@@ -0,0 +1,9 @@
+<?php
+// Thursday, 14th-Aug-2025 - 7:50PM
+// Second try: Monday, 18th-Aug-2025 - 11:27pm
+
+// Take the coordinates of the friend's house
+// divide the coordinates by 5 (the maximum distance in one step)
+// print the ceil() of the division result
+
+echo ceil(readline() / 5);
